50f5139da6e9fee0911611afa150fd9836b089ee
rasulovdilmurodjon06-cmyk <rasulovdilmurodjon06@gmail.com>
2026-04-30 23:11:33 +0500

    Update ADMIN_ID in config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_TOKEN = "8253888597:AAHgdufA4zg1DKmnt_C3mQ19RP9updrLRAQ"
-ADMIN_ID = 123456789 
+ADMIN_ID = 8386486185
 
 REGIONS = [
     "Toshkent", "Andijon", "Buxoro", "Farg'ona", "Jizzax", 
