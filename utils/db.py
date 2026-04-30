62135da614e69d76a47146db9d01c269c9a4fdf7
rasulovdilmurodjon06-cmyk <rasulovdilmurodjon06@gmail.com>
2026-04-30 07:40:29 +0500

    Enhance get_user and get_random_users with comments
    Add comments to clarify user retrieval methods

--- utils/db.py
+++ utils/db.py
@@ -25,10 +25,14 @@ class Database:
         await self.users.update_one({'user_id': user_id}, {'$set': kwargs})
 
     async def get_user(self, user_id):
+        # Foydalanuvchini ID orqali topish
         return await self.users.find_one({'user_id': user_id})
 
     async def get_random_users(self, gender, limit=10):
+        # Tasodifiy foydalanuvchilarni topish
         cursor = self.users.find({'gender': gender})
         users = await cursor.to_list(length=100)
+        if not users:
+            return []
         return random.sample(users, min(len(users), limit))
-                                         
+        
