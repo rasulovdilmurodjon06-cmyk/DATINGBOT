49e4993e837a9bef5b2d92a729b57b0ffc1222d6
rasulovdilmurodjon06-cmyk <rasulovdilmurodjon06@gmail.com>
2026-05-09 08:24:31 +0300

    Add MONGO_URI to config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 API_TOKEN = "8743222556:AAHRWt8Q6retk45JVsbR_BD7TLMR9mgyj0M"
+MONGO_URI = "mongodb+srv://Dimajon:DD1559831DD@cluster0.dty9eag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 ADMIN_ID = 8386486185
 
 REGIONS = [
