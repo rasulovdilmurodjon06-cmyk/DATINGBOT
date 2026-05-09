16caad59ca3e60427ae78bb5bcdc4750e7aab885
rasulovdilmurodjon06-cmyk <rasulovdilmurodjon06@gmail.com>
2026-05-10 00:10:45 +0300

    Change ADMIN_IDS to a list format

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_TOKEN = "8743222556:AAHRWt8Q6retk45JVsbR_BD7TLMR9mgyj0M"
 MONGO_URI = "mongodb+srv://Dimajon:DD1559831DD@cluster0.dty9eag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-ADMIN_IDS = 8386486185
+ADMIN_IDS = [8386486185]
 
 REGIONS = [
     "Toshkent", "Andijon", "Buxoro", "Farg'ona", "Jizzax", 
