6e66eba2c50906aedea6b9804aede96d06500cac
rasulovdilmurodjon06-cmyk <rasulovdilmurodjon06@gmail.com>
2026-05-09 21:42:54 +0300

    Refactor user-related database methods

--- utils/db.py
+++ utils/db.py
@@ -3,47 +3,105 @@ from datetime import datetime
 import random
 
 class Database:
+
     def __init__(self, uri):
+
         self.client = AsyncIOMotorClient(uri)
+
         self.db = self.client['dating_bot']
+
         self.users = self.db['users']
 
     async def add_user(self, user_id, username, lang):
+
         await self.users.update_one(
+
             {'user_id': user_id},
+
             {'$setOnInsert': {
+
                 'user_id': user_id,
                 'username': username,
                 'lang': lang,
                 'is_fake': 0,
                 'created_at': datetime.now()
+
             }},
+
             upsert=True
         )
 
     async def update_user(self, user_id, **kwargs):
-        await self.users.update_one({'user_id': user_id}, {'$set': kwargs})
+
+        await self.users.update_one(
+            {'user_id': user_id},
+            {'$set': kwargs}
+        )
 
     async def get_user(self, user_id):
-        return await self.users.find_one({'user_id': user_id})
 
-    async def get_random_users(self, gender, limit=20):
+        return await self.users.find_one(
+            {'user_id': user_id}
+        )
+
+    async def get_random_users(
+        self,
+        gender,
+        exclude_id=None,
+        limit=20
+    ):
+
+        query = {
+            'gender': gender
+        }
+
+        # O'zini chiqarib tashlash
+        if exclude_id:
+            query['user_id'] = {
+                '$ne': exclude_id
+            }
+
         cursor = self.users.aggregate([
-            {'$match': {'gender': gender}},
-            {'$sample': {'size': limit}}
+            {
+                '$match': query
+            },
+            {
+                '$sample': {
+                    'size': limit
+                }
+            }
         ])
+
         return await cursor.to_list(length=limit)
 
-    async def add_fake_user(self, full_name, age, gender, region, city, photo):
+    async def add_fake_user(
+        self,
+        full_name,
+        age,
+        gender,
+        region,
+        city,
+        photo
+    ):
+
         await self.users.insert_one({
-            'user_id': random.randint(1000, 9999),
+
+            'user_id': random.randint(100000, 999999),
+
             'full_name': full_name,
+
             'age': age,
+
             'gender': gender,
+
             'region': region,
+
             'city': city,
+
             'photo': photo,
+
             'is_fake': 1,
+
             'created_at': datetime.now()
+
         })
-        
